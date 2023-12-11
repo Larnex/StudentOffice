3b3a13bec5796e17fb9197b902354833feb1c164
Larnex <bohdan.asauliuk@gmail.com>
2023-12-11 13:01:49 +0100

    netlify update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify from "vite-plugin-vuetify";
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
